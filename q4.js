a36e0d7532ca824661c2af0d7ecfe1580f7da854
Soumil Maitra <soumilmaitra414@gmail.com>
2023-01-15 21:20:17 +0530

    feat: add 4th question

---
+++ q4.js
@@ -0,0 +1,31 @@
+let str_arr=["we", "lovesedfrf", "code", "academy"];
+function func1(str_arr){
+    let max_len=0;
+    let len=str_arr.length;
+    let ans;
+    for(let i=0;i<len;i++){
+        let str_len=str_arr[i].length
+        if(max_len<str_len){
+            max_len=str_len;
+            ans=str_arr[i];
+        }
+    }
+    return ans
+}
+
+func2 = (str_arr) => {
+    let max_len=0;
+    let len=str_arr.length;
+    let ans;
+    for(let i=0;i<len;i++){
+        let str_len=str_arr[i].length
+        if(max_len<str_len){
+            max_len=str_len;
+            ans=str_arr[i];
+        }
+    }
+    return ans
+} 
+
+console.log(func1(str_arr))
+console.log(func2(str_arr))
