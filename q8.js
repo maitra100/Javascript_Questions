2318d971973668e92cdbe1f38b63e86deaf43cc2
Soumil Maitra <soumilmaitra414@gmail.com>
2023-01-15 22:58:04 +0530

    feat: add 8th question

---
+++ q8.js
@@ -0,0 +1,21 @@
+let str="codeacademy"
+function func1(str){
+    let ans=0,len=str.length;
+    for(let i=0;i<len;i++){
+        if(str[i]=='a' || str[i]=='u' ||str[i]=='o' ||str[i]=='i' ||str[i]=='e')
+        ans++;
+    }
+    return ans;
+}
+
+func2 = (str) => {
+    let ans=0,len=str.length;
+    for(let i=0;i<len;i++){
+        if(str[i]=='a' || str[i]=='u' ||str[i]=='o' ||str[i]=='i' ||str[i]=='e')
+        ans++;
+    }
+    return ans;
+}
+
+console.log(func1(str))
+console.log(func2(str))
